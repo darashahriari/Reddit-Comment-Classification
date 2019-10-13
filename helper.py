5318af3779edead4e3d1081923070a88a31098ee
edward-son <edward_son@live.com>
2019-10-12 21:43:11 -0400

    added pca test with svm

--- helper.py
+++ helper.py
@@ -8,23 +8,28 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
 class Helper:
-	def run_pca(self, train, val):
-		scaler = StandardScaler()
-		train_std = scaler.fit_transform(train)
-		val_std = scaler.fit_transform(val)	
-		pca = PCA().fit(train_std)
+    def run_pca(self, train, val):
+        scaler = StandardScaler()
+        train_std = scaler.fit_transform(train)
+        val_std = scaler.fit_transform(val) 
+        pca = PCA(.95)
+        pca.fit(train_std)
+        print(pca.n_components_)
+        train_img = pca.transform(train_std)
+        test_img = pca.transform(val_std)
+        return train_img, test_img
 
-	def plot(self, x, y, x_label, y_label, title):
-		plt.figure()
-		plt.plot(x,y)
-		plt.xlabel(x_label)
-		plt.ylabel(y_label)
-		plt.suptitle(title)
-	
-	def show(self):
-		plt.show()
+    def plot(self, x, y, x_label, y_label, title):
+        plt.figure()
+        plt.plot(x,y)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.suptitle(title)
+    
+    def show(self):
+        plt.show()
 
-	def correlation_heatmap(self, train, y):
+    def correlation_heatmap(self, train, y):
         total = np.append(train,y,axis=1)
         total = pd.DataFrame(total)
         total.columns = []
@@ -32,4 +37,4 @@ class Helper:
 
         fig, ax = plt.subplots(figsize=(10,10))
         sns.heatmap(correlations, annot=True, cmap="YlGnBu")
-        plt.show()	
+        plt.show()  
