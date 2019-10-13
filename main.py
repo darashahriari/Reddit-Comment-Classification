4fda9ed2a394816b832fa56dd724ab48c90a7530
edward-son <edward_son@live.com>
2019-10-13 00:13:47 -0400

    tfidf vecotorizer and pca

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     helper = Helper()
     data = pd.read_csv('reddit_train.csv')
     # print(data['comments'])
-    vectorizer = CountVectorizer(max_features = 4000, stop_words = stopwords.words('english'))
+    vectorizer = TfidfVectorizer(max_features = 10000, stop_words = stopwords.words('english'))
     train_x = vectorizer.fit_transform(data['comments'])
     train_y = vectorizer.fit_transform(data['subreddits'])
     print(len(vectorizer.get_feature_names()))
@@ -37,20 +37,15 @@ if __name__ == '__main__':
     training_y = train_y.toarray()
     num_class = len(vectorizer.get_feature_names())
     num_feature = train_x.toarray().shape[1]
-    #pca = helper.run_pca(training_x, training_y)
-    #print(pca)
     X_train, X_test, y_train, y_test = train_test_split(training_x, training_y, test_size=0.33, random_state=42)
     X_train, X_test = helper.run_pca(X_train,X_test)
 
-    '''model_bayes = NaiveBayes(training_x=X_train,
+    model_bayes = NaiveBayes(training_x=X_train,
                              training_y=y_train,
                              num_class=num_class,
                              theta_k=np.full((num_class, 1), 0.0),
                              theta_j_k=np.full((num_feature, 20), 0.0),
                              num_feature=num_feature)
     model_bayes.fit()
-    y_pred = model_bayes.predict(X_test)'''
-    clf = OneVsRestClassifier(LinearSVC(random_state=0, tol=1e-5))
-    clf.fit(X_train, y_train) 
-    y_pred = clf.predict(X_test)
+    y_pred = model_bayes.predict(X_test)
     print(accuracy_score(y_test, y_pred))
