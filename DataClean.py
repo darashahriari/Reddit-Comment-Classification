718cf75b8caf9a30b17939d30cd97eefacddb508
edward-son <edward_son@live.com>
2019-10-18 16:34:45 -0400

    clean up code

--- DataClean.py
+++ DataClean.py
@@ -9,69 +9,72 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.linear_model import LogisticRegression,SGDClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import LinearSVC
 from sklearn.feature_extraction.text import TfidfVectorizer
 import string
-
+from sklearn import linear_model
+from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import SGDClassifier
 
 class DataClean :
     def __init__(self, filePath):
         self.filePath = filePath
-    
-    
-
-    def tokenization(self, text):
-        text = re.split('\W', text)
-        return text
-    
-    def remove_stopwords(self, text):
-        stopword = nltk.corpus.stopwords.words('english')
-        text = [word for word in text if word not in stopword]
-        return text
-
-    def stemming(self, text):
-        ps = nltk.PorterStemmer()
-        text = [ps.stem(word) for word in text]
-        return text
-
-    def lemmatizer(self, text):
-        wn = nltk.WordNetLemmatizer()
-        text = [wn.lemmatize(word) for word in text]
-        return text
-
 
     def read(self):
+        print('\n \n \n Reading File ...')
+
         data = pd.read_csv(self.filePath)
-        y = data['subreddits']
-        Lemmatizer = WordNetLemmatizer()
+
+        print('Cleaning Text ...')
+
+        self.y = data['subreddits']
+        X = data['comments']
+        stemmer = WordNetLemmatizer()
         documents = []
-        data['comments'] = data['comments'].apply(lambda x: self.tokenization(x.lower()))
-        data['comments'] = data['comments'].apply(lambda x: self.remove_stopwords(x))
-        data['comments'] = data['comments'].apply(lambda x: self.stemming(x))
-        data['comments'] = data['comments'].apply(lambda x: self.lemmatizer(x))
-
-
-        for comment in data['comments']:
-            newLine = ""
-            for word in comment:
-                if(len(word) > 1):
-                    newLine = newLine + ' '+ Lemmatizer.lemmatize(word.lower()) 
+        for sen in range(0, len(X)):
+            # Remove all the special characters
+            document = re.sub(r'\W', ' ', str(X[sen]))
+            # remove all single characters
+            document = re.sub(r'\s+[a-zA-Z]\s+', ' ', document)
+            # Remove single characters from the start
+            document = re.sub(r'\^[a-zA-Z]\s+', ' ', document) 
+            # Substituting multiple spaces with single space
+            document = re.sub(r'\s+', ' ', document, flags=re.I)
+            # Removing prefixed 'b'
+            document = re.sub(r'^b\s+', '', document)
+            # Converting to Lowercase
+            document = document.lower()
+            # Lemmatization
+            document = document.split()
+            document = [stemmer.lemmatize(word) for word in document]
+            document = ' '.join(document)
+            documents.append(document)
             
-            documents.append(newLine)
+        self.X = documents
+
+    def featureSelect(self):
+
+        print('Selecting Features ...')
         
-        #print(documents)
-        
-        vectorizer = TfidfVectorizer(min_df=2, max_df=0.03, stop_words='english')
-        X = vectorizer.fit_transform(documents).toarray()
-        self.vocabulary = vectorizer.get_feature_names()
+        self.tfidf_vectorizer = TfidfVectorizer(min_df=2, max_df=0.03, stop_words='english')
+
+        X = self.tfidf_vectorizer.fit_transform(self.X)
+
         self.xtrain = X
-        self.ytrain = y
+        self.ytrain = self.y
+
+        print('Feature Selection Complete:')
+        print('Starting Taining ...\n \n')
         
+        self.text_clf = Pipeline([('tfidf', TfidfTransformer()),('clf', MultinomialNB(alpha=.83,fit_prior=True, class_prior=[.05,.05,.05,.05,.05,.05,.05,.05,.05,.05,.05,.05,.05,.05,.05,.05,.05,.05,.05,.05])),]) 
+        
+
     def fit(self):
-        self.classifier = MultinomialNB()
-        self.classifier.fit(self.xtrain, self.ytrain) 
+        self.text_clf.fit(self.xtrain, self.ytrain)
 
     def pred(self, filepath):
         testData = pd.read_csv(filepath)
@@ -97,30 +100,32 @@ class DataClean :
             document = ' '.join(document)
             documents.append(document)
         
-        vectorizer = TfidfVectorizer( vocabulary=self.vocabulary)
-        X = vectorizer.fit_transform(documents).toarray()
+        X = self.tfidf_vectorizer.transform(documents)
 
-        predictions = self.classifier.predict(X)
+        self.xtest = X
+
+        predictions = self.text_clf.predict(self.xtest)
         df = pd.DataFrame({'Category': predictions})
-        df.to_csv(index=True, path_or_buf='ans.csv')
+        df.to_csv(index=True, path_or_buf='/Users/dara/desktop/mini project 2/ans.csv')
 
     def fitTest(self):
         self.xtrain, self.xtest, self.ytrain, self.ytest = train_test_split(self.xtrain, self.ytrain, test_size=0.2, random_state=0)
+        #self.gs_clf = GridSearchCV(self.text_clf, self.parameters, cv=5, iid=False, n_jobs=-1)
+        #self.gs_clf.fit(self.xtrain, self.ytrain)
+        self.text_clf.fit(self.xtrain, self.ytrain)
         #self.classifier = MultinomialNB()
-        #self.classifier = LogisticRegression(random_state=0, solver='newton-cg', multi_class='multinomial')
-        #self.classifier = LinearSVC(random_state=0, tol=1e-5)
-        self.classifier = SGDClassifier(max_iter=1000, tol=1e-3)
-        self.classifier.fit(self.xtrain, self.ytrain) 
+        #self.classifier.fit(self.xtrain, self.ytrain) 
 
     def predTest(self):
-        self.ypred = self.classifier.predict(self.xtest)
-        print(confusion_matrix(self.ytest,self.ypred))
+        #self.ypred = self.gs_clf.predict(self.xtest)
+        self.ypred =  self.text_clf.predict(self.xtest)
+        #self.ypred = self.classifier.predict(self.xtest)
         print(classification_report(self.ytest,self.ypred))
         print(accuracy_score(self.ytest, self.ypred))
         
-cleaner = DataClean("reddit_train.csv")
+cleaner = DataClean("/Users/dara/desktop/mini project 2/reddit_train.csv")
 cleaner.read()
-print("done read")
+cleaner.featureSelect()
 cleaner.fitTest()
 cleaner.predTest()
 #cleaner.fit()
--- StackingClassifier.py
+++ StackingClassifier.py
@@ -3,22 +3,19 @@ import numpy as np
 import numpy.linalg
 import numpy.random
 import pandas as pd
-from NaiveBayes import NaiveBayes
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression,SGDClassifier
-from mlxtend.classifier import StackingClassifier, StackingCVClassifier
+from mlxtend.classifier import StackingClassifier
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.svm import LinearSVC, SVC
+from sklearn.svm import  SVC
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import normalize
 from sklearn import metrics
-from helper import Helper
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
 from sklearn import model_selection
 from sklearn.neural_network import MLPClassifier
-from sklearn.neighbors import KNeighborsClassifier
+import time
 
 if __name__ == '__main__':
     # import data
@@ -29,7 +26,6 @@ if __name__ == '__main__':
     val_x = val['comments']
     X = data['comments']
     y = data['subreddits']
-    helper = Helper()
 
     # tf idf
     tf_idf = TfidfVectorizer(sublinear_tf=True, max_df = 0.03, min_df=2, norm='l2',encoding='latin-1', ngram_range=(1, 2),stop_words='english')
@@ -44,8 +40,7 @@ if __name__ == '__main__':
     chi2_features = SelectKBest(chi2, k = 15500)
     X_kbest_features = chi2_features.fit_transform(X,y)
     val_kbest_features = chi2_features.transform(val_x)
-    print('Original feature number:', X.shape[1])
-    print('Reduced feature number:', X_kbest_features.shape[1])
+
     train_x_normalize, test_x_normalize, train_y, test_y = train_test_split(X_kbest_features,y, train_size=0.8,
                                                                     test_size=0.2)    
 
@@ -57,15 +52,22 @@ if __name__ == '__main__':
                           use_probas=True,
                           average_probas=False,
                           meta_classifier=clf1)
-    for clf, label in zip([clf2, clf3, sclf], 
+
+    #score all classifiers
+    for clf, classifier in zip([clf2, clf3, sclf], 
                       [
                        'nb',
                        'svm',
                        'StackingClassifier']):
         scores = model_selection.cross_val_score(clf,X_kbest_features , y, cv=5, scoring='accuracy')
-        print("Accuracy: %0.4f (+/- %0.4f) [%s]" % (scores.mean(), scores.std(), label))
+        print("Accuracy: %0.4f (+/- %0.4f) [%s]" % (scores.mean(), scores.std(), classifier))
     
-    sclf.fit(train_x_normalize, train_y)
+    #fit training data
+    start_time = time.time()
+    sclf.fit(X_kbest_features, y)
+    print("--- %s runtime in seconds ---" % (time.time() - start_time))
+
+    #predict validation classes
     val_pred = sclf.predict(val_kbest_features)
 
     #write to validation file
